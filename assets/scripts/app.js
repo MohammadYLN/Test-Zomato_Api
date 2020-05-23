f50ebbd2aa64470e001ad8a2e74d2d3ca96458ef
Mohammad Yalaan <mohamadyalaan@gmail.com>
2020-05-23 19:19:30 +0430

    fixing problem in refresh page when select a restaurant

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,6 +7,7 @@ const leftItems = document.querySelectorAll(".left-items");
 const imageSection = document.querySelector(".img-div");
 
 
+
 /* --------------------------------- show list of Categories ----------------------------- */
 async function getCategories(){
   fetch("https://developers.zomato.com/api/v2.1/categories", {
@@ -59,7 +60,8 @@ async function getCuisines(){
 
 /* ---------------------------------  list of restaurants ----------------------------- */
 async function showRests(){
-    fetch("https://developers.zomato.com/api/v2.1/search", {
+  let url = "https://developers.zomato.com/api/v2.1/search";
+    fetch(url, {
     headers: { "user-key": "6c11b6f8975b808590064388ed3f37a8" }
   }).
   then(response => response.json()).
@@ -75,12 +77,14 @@ async function showRests(){
         // console.log(id);
       }
   })
+  
 }
 
 /* ---------------------------------  select and show one of restaurants ----------------------------- */
 
 async function getSelectedRest(id){
-  fetch(`https://developers.zomato.com/api/v2.1/restaurant?res_id=${id}`, {
+  let url = `https://developers.zomato.com/api/v2.1/restaurant?res_id=${id}`;
+  fetch(url, {
     headers: { "user-key": "6c11b6f8975b808590064388ed3f37a8" }
   })
   .then(res => res.json())
@@ -88,8 +92,8 @@ async function getSelectedRest(id){
     
     const{location:{address},photos} = resData;
     // console.log(photosUrl);
-    let txtElmnt = "";
-     txtElmnt = `<h1 class="desc-title" data-id="${resData.id}">${resData.name}</h2>
+    
+    let txtElmnt = `<h1 class="desc-title" data-id="${resData.id}">${resData.name}</h2>
     <p class="desc-address" data-id="${resData.id}">${address}</p>
     <p class="desc-booking" data-id="${resData.id}">Paragraph booking</p>
     <p class="desc-delivery" data-id="${resData.id}">Paragraph delivery</p>
@@ -102,8 +106,8 @@ async function getSelectedRest(id){
 
     let imgElmnt = `<img class="img-show" src="${resData.photos[0].photo.url}" />`;
 
-    descSection.innerHTML = descSection.innerHTML + txtElmnt;
-    imageSection.innerHTML = imageSection.innerHTML + imgElmnt;
+    descSection.innerHTML = txtElmnt;
+    imageSection.innerHTML =  imgElmnt;
 
   }).catch((err)=>console.log(err));
   
@@ -111,7 +115,7 @@ async function getSelectedRest(id){
 
 
 function showSelectedRest(event){
- 
+  
   let showId = event.target.getAttribute('data-id');
   console.log(showId);
   getSelectedRest(showId);
